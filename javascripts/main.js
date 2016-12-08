847f76cd6ddc333d4d20769bb7395e834407b6f3
Fisayomi <fisayomi@users.noreply.github.com>
2016-12-08 22:41:57 +0100

    Completed Quiz Application

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,8 +6,6 @@
 function checkName(){
       var user = document.getElementById('username').value;
       var password = document.getElementById('password').value;
-     
-      
         var ref = firebase.database().ref("users");
         ref.once("value")
           .then(function(snapshot) {
@@ -36,10 +34,11 @@ function checkName(){
                window.location.href = 'homePage.html';
             }
             else if(!found){
-              alert('You are not a registered user, Kindly register to continue');
+              document.getElementById('in').innerHTML = "You are not a registered user, Kindly register to continue";
+              //alert("You are not a registered user, Kindly register to continue");
             }
             else if(!correctPassword){
-              alert("Password Incorrect");
+              document.getElementById('in').innerHTML = "Incorrect Password";
             }
 
             window.sessionStorage.setItem("user", user);
@@ -48,42 +47,5 @@ function checkName(){
           
      }
 
-function authenticate(){
-  var email = document.getElementById('email').value;
-  var password = document.getElementById('password2').value;
-  console.log("I am creating a new user");
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        if (errorCode == 'auth/weak-password') {
-          alert('The password is too weak.');
-        } else {
-          alert(errorMessage);
-        }
-        console.log(error);
-      });
 
-}
-
-function getUser(){
-
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      console.log(user.email, "email \n", user.uid, "user id");
-    } else {
-      // No user is signed in.
-    }
-  });
-}
-
-var user = firebase.auth().currentUser;
-var name, email, photoUrl, uid;
-
-if (user != null) {
-  name = user.displayName;
-  email = user.email;
-}
 
